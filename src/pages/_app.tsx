cb77efc352ed76c84d5a322375ca1d26d219b8fb
Sina Nikmaram <NikmaramCareer@gmail.com>
2023-04-30 00:15:31 -0400

    Update App Head element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Sample Web App Title</title>
+        <meta name="description" content="Sample Web App Description" />
+        <meta
+          name="keywords"
+          content="keywords, for, SEO, separated, by, commas"
+        />
+        <meta name="author" content="Your Name or Company Name" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
